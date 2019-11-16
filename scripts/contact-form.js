d5b2e9cb247e66dd970fed44d85069aede60e79b
Stewie02 <stefansijbesma12@gmail.com>
2019-11-16 13:41:14 +0200

    Fixed form

--- scripts/contact-form.js
+++ scripts/contact-form.js
@@ -3,36 +3,40 @@ function send() {
     let statusElement = document.getElementById('contact-status');
     
     let sendMail = function() {
-      let nameElement = document.getElementById('contact-name');
-      let emailElement = document.getElementById('contact-email');
-      let messageElement = document.getElementById('contact-message');
+      var nameElement = document.getElementById('contact-name');
+      var emailElement = document.getElementById('contact-email');
+      var messageElement = document.getElementById('contact-message');
 
-      let messageHTML = messageElement.value;
+      var messageHTML = messageElement.value;
 
-      let params = {
+      var params = {
         'from_name': nameElement.value,
         'from_email': emailElement.value,
         'message_html': messageHTML
-      }
-
-      let serviceId = 'gmail';
-      let templateId = 'template_lpIHse0c';
+      };
+  
+      let serviceId = 'gmail_fgc';
+      let templateId = 'template_TCzXplGq';
+      
 
       emailjs.send(serviceId, templateId, params)
         .then(function(response) {
           statusElement.innerText = 'A message has been sent to our team! We will review your message soon!'
         }, function(error) {
           statusElement.innerText = 'Could not send a message... Please try again later!'
+          ;
         });
-    }
-    
-    if (lastSent == null || Date.now() > lastSent + 600000) {
+      }
+
+    if (isNaN(lastSent) || Date.now() > lastSent + 600000) {
       sendMail();
       localStorage.setItem('contact-last-sent', Date.now());
 
       statusElement.innerText = 'Please wait...'
     } else {
       statusElement.innerText = 'Could not send a message... Please try again later!'
+      localStorage.setItem('contact-last-sent', Date.now());
+
     }
 
   }
